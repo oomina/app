48eb7cb2d3f6fccdc4b244d68d4be86bd54f56c4
ohmina <orange9751@naver.com>
2023-09-13 04:50:05 +0900

    리액트 커밋 완료

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import logo from './logo.svg';
+import './App.css';
+
+const Container = styled.div`
+  text-align: right;
+`;
+
+function App() {
+  return (
+    <Container>
+      <div className="App">
+        OhMina
+      </div>
+    </Container>
+  );
+}
+
+export default App;
